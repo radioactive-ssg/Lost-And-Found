e7d3fb787cbba861843e0d19f324fe8de5092413
radioactive-ssg <sanketgawande989@gmail.com>
2025-04-11 19:34:03 +0530

    Update config.js / Changes API

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,4 +1,4 @@
 // export const api = "https://dull-gold-clam-tutu.cyclic.app"
-// export const api = "https://lostandfound-api.onrender.com";
-export const api = "http://localhost:10000";
+export const api = "https://manit-lost-and-found.onrender.com";
+// export const api = "http://localhost:10000";
 // export const api = "https://lost-and-found-api-nine.vercel.app";
